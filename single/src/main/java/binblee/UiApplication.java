3ae17f1ebcbef02b01649ac8af0e53b647f8ae6f
Li Bin <binblee@users.noreply.github.com>
2015-07-18 09:41:35 +0800

    csrf filter, login/logoff sucess

--- single/src/main/java/binblee/UiApplication.java
+++ single/src/main/java/binblee/UiApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,12 +33,22 @@ public class UiApplication {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.httpBasic()
-                .and()
-                .authorizeRequests()
+                .and().authorizeRequests()
                 .antMatchers("/index.html", "/home.html", "/login.html", "/","/webjars/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().csrf().csrfTokenRepository(csrfTokenRepository())
+                .and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+            ;
         }
+
+        private CsrfTokenRepository csrfTokenRepository(){
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setHeaderName("X-XSRF-TOKEN");
+            return repository;
+        }
+
     }
+
 
     @RequestMapping("/resource")
     public Map<String,Object> home(){
