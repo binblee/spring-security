896fe779f1ffa5e5e933a89a0c0521c7f052af97
Li Bin <binblee@users.noreply.github.com>
2015-07-20 17:18:48 +0800

    initial code for classic web example

---
+++ classicweb/src/main/java/demo/MvcConf.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by libin on 15/7/20.
+ */
+
+@Configuration
+public class MvcConf extends WebMvcConfigurerAdapter{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
---
+++ classicweb/src/main/java/demo/controller/DemoController.java
@@ -0,0 +1,11 @@
+package demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by libin on 15/7/20.
+ */
+
+@Controller
+public class DemoController {
+}
